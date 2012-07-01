28fa6849a582ca5a0299bc1f02ad105495906fab
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-07-01 19:43:14 +0200

    Added set habtm task

--- lib/set_habtm_for_rodamoto.rb
+++ lib/set_habtm_for_rodamoto.rb
@@ -7,53 +7,53 @@ include ActionDispatch::TestProcess
 class SetHabtmForRodamoto
 
   def initialize()
-    file = "/home/jose/RubymineProjects/habtm.csv"
-    file_final = "/home/jose/RubymineProjects/habtm_final.csv"
-    total = []
-    row_new = []
+    @file = "/home/jose/RubymineProjects/habtm.csv"
+    @file_final = "/home/jose/RubymineProjects/habtm_final.csv"
+    @total = []
+    @row_new = []
   end
   
   def run
-    CSV.foreach(file) do |row|
+    CSV.foreach(@file) do |row|
       if row[0] == ""
-        row_new[0] = ""
+        @row_new[0] = ""
       else
-        row_new[0] = Spree::TireWidth.find_by_name(row[0]).id
+        @row_new[0] = Spree::TireWidth.find_by_name(row[0]).id
       end
       if row[1] == ""
-        row_new[1] = ""
+        @row_new[1] = ""
       else
-        row_new[1] = Spree::TireProfile.find_by_name(row[1]).id
+        @row_new[1] = Spree::TireProfile.find_by_name(row[1]).id
       end
       if row[2] == ""
-        row_new[2] = ""
+        @row_new[2] = ""
       else
-        row_new[2] = Spree::TireInnertube.find_by_name(row[2]).id
+        @row_new[2] = Spree::TireInnertube.find_by_name(row[2]).id
       end
       if row[3] == ""
-        row_new[3] = ""
+        @row_new[3] = ""
       else
-        row_new[3] = Spree::TireIc.find_by_name(row[3]).id
+        @row_new[3] = Spree::TireIc.find_by_name(row[3]).id
       end
       if row[4] == ""
-        row_new[4] = ""
+        @row_new[4] = ""
       else
-        row_new[4] = Spree::TireSpeedCode.find_by_name(row[4]).id
+        @row_new[4] = Spree::TireSpeedCode.find_by_name(row[4]).id
       end
       if row[5] == ""
-        row_new[5] = ""
+        @row_new[5] = ""
       else
-        row_new[5] = Spree::TireFr.find_by_name(row[5]).id
+        @row_new[5] = Spree::TireFr.find_by_name(row[5]).id
       end
       if row[6] == ""
-        row_new[6] = ""
+        @row_new[6] = ""
       else
-        row_new[6] = Spree::TireTttl.find_by_name(row[6]).id
+        @row_new[6] = Spree::TireTttl.find_by_name(row[6]).id
       end
-      total << row_new
+      @total << @row_new
     end
-    CSV.open(file_final, "wb") do |csv|
-      total.each do |row|
+    CSV.open(@file_final, "wb") do |csv|
+      @total.each do |row|
         csv << row
       end
     end
