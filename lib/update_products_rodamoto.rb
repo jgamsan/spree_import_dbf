23a1dd8fa67fa3ada4e78d78c9016478eee17d81
jose miguel <miguel.gamazo@galiclick.com>
2012-07-08 10:30:00 +0200

    Mejoras en update_rodamoto

--- lib/update_products_rodamoto.rb
+++ lib/update_products_rodamoto.rb
@@ -62,14 +62,205 @@ class UpdateProductsRodamoto
   end
   
   def delete_items_action(list)
-    puts "ejecutado accion borrar"
+    puts "ejecutando accion borrar"
+    i = 0
+    unless list.empty?
+      list.each do |element|
+        articulo = Spree::Variant.find_by_sku(element)
+        producto = articulo.product
+        product.destroy
+        i += 1
+      end
+    end
+    puts "Borrados #{i} articulos"
   end
   
   def create_items_action(list)
-    puts "ejecutado accion crear"
+    puts "ejecutando accion crear"
+    i = 0
+    unless list.empty
+      list.each do |element|
+        CSV.foreach(@articulos) do |row|
+          if row[1] == element
+            @product = Spree::Product.new
+            @product.name = row[2]
+            @product.permalink = row[2].downcase.gsub(/\s+/, '-').gsub(/[^a-zA-Z0-9_]+/, '-')
+            @product.count_on_hand = row[22]
+            
+            @product.sku = row[1]
+            @product.price = row[52]
+            @product.cost_price = row[6]
+            @product.available_on = Date.today - 1.day
+            @product.tire_width_id = set_width(row)
+            @product.tire_profile_id = set_profile(row)
+            @product.tire_innertube_id = set_innertube(row)
+            @product.tire_ic_id = set_ic(row)
+            @product.tire_speed_code_id = set_speed_code(row)
+            @product.tire_fr_id = set_fr(row)
+            @product.tire_tttl_id = set_tttl(row)
+            
+            @product.pvp3 = row[52] * 1.18
+            @product.pvp7 = row[56] * 1.18
+            @product.pvp9 = row[58] * 1.18
+            @product.pvp12 = row[92] * 1.18
+            
+            @product.taxons << Spree::Taxon.find(set_catalog(row[84].to_i, row[83].to_i, row[85].to_i))
+            
+            @product.taxons << Spree::Taxon.find(set_brand(row[86].to_i)) unless row[86].to_i == 0
+            if @product.save!
+              puts "Creado articulo #{row[1]}"
+              i += 1
+            end
+          end
+        end
+      end
+    end
   end
   
   def update_items_action(list)
-    puts "ejecutado accion actualizar"
+    puts "ejecutando accion actualizar"
+    i = 0
+    total = list.count
+    unless list.empty?
+      list.each do |element|
+        CSV.foreach(@articulos) do |row|
+          if row[1] == element 
+            articulo = Spree::Variant.find_by_sku(element)
+            producto = articulo.product
+            producto.update_attributes(
+              :name = row[2],
+              :permalink = row[2].downcase.gsub(/\s+/, '-').gsub(/[^a-zA-Z0-9_]+/, '-'),
+              :count_on_hand = row[22],
+              :sku = row[1],
+              :price = row[52],
+              :cost_price = row[6],
+              :available_on = Date.today - 1.day,
+              :tire_width_id = set_width(row),
+              :tire_profile_id = set_profile(row),
+              :tire_innertube_id = set_innertube(row),
+              :tire_ic_id = set_ic(articulo),
+              :tire_speed_code_id = set_speed_code(row),
+              :tire_fr_id = set_fr(row),
+              :tire_tttl_id = set_tttl(row),
+              :pvp3 = row[52] * 1.18,
+              :pvp7 = row[56] * 1.18,
+              :pvp9 = row[58] * 1.18,
+              :pvp12 = row[92] * 1.18
+            )
+            puts "Actualizado producto #{i} de #{total} productos"
+          end
+        end
+      end
+    end
+  end
+  
+  def set_catalog(clasub, clatipart, clacat)
+    if clacat == 21
+      case clasub
+        when 28
+          10
+        when 29
+          4
+        when 30
+          13
+      end
+    else
+      case clatipart
+        when 73
+          case clasub
+            when 28
+              10
+            when 29
+              5
+            when 30
+              14
+          end
+        when 81
+          6
+        when 82
+          6
+        when 83
+          7
+        when 80
+          8
+        when 64
+          16
+        when 79
+          17
+        when 65
+          18
+        when 77
+          19
+        when 78
+          20
+        when 85
+          21
+        when 84
+          22
+      end
+    end
+  end
+  
+  def get_catalog(clasub, clatipart, clacat)
+    if clacat == 21
+      if @clasub.include?(clasub)
+        return false
+      else
+        return true
+      end
+    else
+      if clatipart == 73
+        if @clasub.include?(clasub)
+          return false
+        else
+          return true
+        end
+      else
+        if @clatipart.include?(clatipart)  
+          return false
+        else
+          return true
+        end
+      end
+    end
+  end
+  
+  def set_brand(clamar)
+    22 + clamar.to_i
+  end
+  
+  def set_width(articulo)
+    ancho = articulo[113]
+    ancho == "" ? ancho : Spree::TireWidth.find_by_name(ancho).id
+  end
+  
+  def set_profile(articulo)
+    perfil = articulo[115]
+    perfil == "" ? perfil : Spree::TireProfile.find_by_name(perfil).id
+  end
+  
+  def set_innertube(articulo)
+    llanta = articulo[117]
+    llanta == "" ? llanta : Spree::TireInnertube.find_by_name(llanta).id
+  end
+  
+  def set_ic(articulo)
+    ic = articulo[118]
+    ic == "" ? ic : Spree::TireIc.find_by_name(ic).id
+  end
+  
+  def set_speed_code(articulo)
+    vel = articulo[119]
+    vel == "" ? vel : Spree::TireSpeedCode.find_by_name(vel).id
+  end
+  
+  def set_fr(articulo)
+    fr = articulo[120]
+    fr == "" ? fr : Spree::TireFr.find_by_name(fr).id
+  end
+  
+  def set_tttl(articulo)
+    tttl = articulo[121]
+    tttl == "" ? tttl : Spree::TireTttl.find_by_name(tttl).id
   end
 end
