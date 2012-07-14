f9d289a0d75a196ceca41e5b955da44251c3b15a
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-14 16:41:17 +0200

    mas mejoras en update

--- lib/update_products_rodamoto.rb
+++ lib/update_products_rodamoto.rb
@@ -35,14 +35,22 @@ class UpdateProductsRodamoto
     inter = @list & @list_updated
     borrar = @list - @list_updated
     nuevos = @list_updated - @list
-    
+    actualizar = crear = []
     puts "Numero de articulos a actualizar #{inter.count}"
     puts "Numero de articulos a borrar #{borrar.count}"
     puts "Numero de articulos nuevos #{nuevos.count}"
-    
+    puts "Recargando Arrays"
+    CSV.foreach(@articulos, {headers: true}) do |row|
+      if inter.include?(row[1])
+        actualizar << row
+      end
+      if nuevos.include?(row[1])
+        crear << row
+      end
+    end
     #delete_items_action(borrar)
     #create_items_action(nuevos)
-    update_items_action(inter)
+    update_items_action(actualizar)
   end
   
   def get_catalog(clasub, clatipart, clacat)
@@ -88,38 +96,34 @@ class UpdateProductsRodamoto
     i = 0
     unless list.empty?
       list.each do |element|
-        CSV.foreach(@articulos) do |row|
-          if row[1] == element
-            @product = Spree::Product.new
-            @product.name = row[2]
-            @product.permalink = row[2].downcase.gsub(/\s+/, '-').gsub(/[^a-zA-Z0-9_]+/, '-')
-            @product.count_on_hand = row[22]
-            
-            @product.sku = row[1]
-            @product.price = row[52]
-            @product.cost_price = row[6]
-            @product.available_on = Date.today - 1.day
-            @product.tire_width_id = set_width(row)
-            @product.tire_profile_id = set_profile(row)
-            @product.tire_innertube_id = set_innertube(row)
-            @product.tire_ic_id = set_ic(row)
-            @product.tire_speed_code_id = set_speed_code(row)
-            @product.tire_fr_id = set_fr(row)
-            @product.tire_tttl_id = set_tttl(row)
-            
-            @product.pvp3 = row[52] * 1.18
-            @product.pvp7 = row[56] * 1.18
-            @product.pvp9 = row[58] * 1.18
-            @product.pvp12 = row[92] * 1.18
-            
-            @product.taxons << Spree::Taxon.find(set_catalog(row[84].to_i, row[83].to_i, row[85].to_i))
-            
-            @product.taxons << Spree::Taxon.find(set_brand(row[86].to_i)) unless row[86].to_i == 0
-            if @product.save!
-              puts "Creado articulo #{row[1]}"
-              i += 1
-            end
-          end
+        @product = Spree::Product.new
+        @product.name = row[2]
+        @product.permalink = row[2].downcase.gsub(/\s+/, '-').gsub(/[^a-zA-Z0-9_]+/, '-')
+        @product.count_on_hand = row[22]
+        
+        @product.sku = row[1]
+        @product.price = row[52]
+        @product.cost_price = row[6]
+        @product.available_on = Date.today - 1.day
+        @product.tire_width_id = set_width(row)
+        @product.tire_profile_id = set_profile(row)
+        @product.tire_innertube_id = set_innertube(row)
+        @product.tire_ic_id = set_ic(row)
+        @product.tire_speed_code_id = set_speed_code(row)
+        @product.tire_fr_id = set_fr(row)
+        @product.tire_tttl_id = set_tttl(row)
+        
+        @product.pvp3 = row[52] * 1.18
+        @product.pvp7 = row[56] * 1.18
+        @product.pvp9 = row[58] * 1.18
+        @product.pvp12 = row[92] * 1.18
+        
+        @product.taxons << Spree::Taxon.find(set_catalog(row[84].to_i, row[83].to_i, row[85].to_i))
+        
+        @product.taxons << Spree::Taxon.find(set_brand(row[86].to_i)) unless row[86].to_i == 0
+        if @product.save!
+          puts "Creado articulo #{row[1]}"
+          i += 1
         end
       end
     end
@@ -131,47 +135,42 @@ class UpdateProductsRodamoto
     i = 0
     total = list.count
     unless list.empty?
-      list.each do |element|
-        CSV.foreach(@articulos) do |row|
-          if row[1] == element
-            #n = []
-            codigo = row[1]
-            ActiveRecord::Base.connection.execute("update spree_variants set pvp3 = #{row[52] * 1.18}, pvp7 = #{row[52] * 1.18}, pvp9 = #{row[58] * 1.18}, pvp12 = #{row[92] * 1.18} where sku = #{'codigo'}")
-            #articulo = Spree::Variant.find_by_sku(element)
-            #producto = articulo.product
-            #producto.update_attributes(
-              #:name => row[2],
-              #:permalink => row[2].downcase.gsub(/\s+/, '-').gsub(/[^a-zA-Z0-9_]+/, '-'),
-              #:count_on_hand => row[22].to_i,
-              #:sku => row[1],
-              #:price => row[52],
-              #:cost_price => row[6],
-              #:available_on => Date.today - 1.day,
-              #:tire_width_id => set_width(row),
-              #:tire_profile_id => set_profile(row),
-              #:tire_innertube_id => set_innertube(row),
-              #:tire_ic_id => set_ic(row),
-              #:tire_speed_code_id => set_speed_code(row),
-              #:tire_fr_id => set_fr(row),
-              #:tire_tttl_id => set_tttl(row),
-             # :pvp3 => row[52] * 1.18,
-             # :pvp7 => row[56] * 1.18,
-             # :pvp9 => row[58] * 1.18,
-             # :pvp12 => row[92] * 1.18
-            #)
-            #t = producto.taxons.map {|x| x.id}
-            #n << set_catalog(row[84].to_i, row[83].to_i, row[85].to_i)
-            #n << set_brand(row[86].to_i) unless row[86].to_i == 0
-            #unless t == n
-            #  producto.taxons.delete_all
-            #  producto.taxons << Spree::Taxon.find(set_catalog(row[84].to_i, row[83].to_i, row[85].to_i))
-            #  producto.taxons << Spree::Taxon.find(set_brand(row[86].to_i)) unless row[86].to_i == 0
-            #end
-            print "Actualizado producto #{i} de #{total} productos"
-            print "\r"
-            i += 1
-          end
+      list.each do |row|  
+        n = []
+        codigo = row[1]
+        articulo = Spree::Variant.find_by_sku(row[1])
+        producto = articulo.product
+        producto.update_attributes(
+          :name => row[2],
+          :permalink => row[2].downcase.gsub(/\s+/, '-').gsub(/[^a-zA-Z0-9_]+/, '-'),
+          :count_on_hand => row[22].to_i,
+          :sku => row[1],
+          :price => row[52],
+          :cost_price => row[6],
+          :available_on => Date.today - 1.day,
+          :tire_width_id => set_width(row),
+          :tire_profile_id => set_profile(row),
+          :tire_innertube_id => set_innertube(row),
+          :tire_ic_id => set_ic(row),
+          :tire_speed_code_id => set_speed_code(row),
+          :tire_fr_id => set_fr(row),
+          :tire_tttl_id => set_tttl(row),
+          :pvp3 => row[52] * 1.18,
+          :pvp7 => row[56] * 1.18,
+          :pvp9 => row[58] * 1.18,
+          :pvp12 => row[92] * 1.18
+        )
+        t = producto.taxons.map {|x| x.id}
+        n << set_catalog(row[84].to_i, row[83].to_i, row[85].to_i)
+        n << set_brand(row[86].to_i) unless row[86].to_i == 0
+        unless t == n
+          producto.taxons.delete_all
+          producto.taxons << Spree::Taxon.find(set_catalog(row[84].to_i, row[83].to_i, row[85].to_i))
+          producto.taxons << Spree::Taxon.find(set_brand(row[86].to_i)) unless row[86].to_i == 0
         end
+        print "Actualizado producto #{i} de #{total} productos"
+        print "\r"
+        i += 1
       end
     end
   end
